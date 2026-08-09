7b508335e5a0b2ff9599f1ee24e26141738b3518
1saacromer <155871160+1saacromer@users.noreply.github.com>
2026-08-08 22:10:57 -0700

    Add: two-integer-sum - submission-10

---
+++ Data Structures & Algorithms/two-integer-sum/submission-10.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        for i in range(len(nums)):
+            key = target - nums[i]
+            if key in nums[i+1:]:
+                return [i, nums.index(key, i+1, len(nums))]
+        
